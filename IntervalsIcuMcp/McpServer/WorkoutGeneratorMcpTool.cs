e4a82e2aa95ead4d87cd7c45007e82f7221a6f42
colinnuk <colinnuk@users.noreply.github.com>
2025-11-15 08:14:38 -0800

    Clarify use of WorkoutGeneratorTool

--- IntervalsIcuMcp/McpServer/WorkoutGeneratorMcpTool.cs
+++ IntervalsIcuMcp/McpServer/WorkoutGeneratorMcpTool.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using IntervalsIcuMcp.Services;
 using IntervalsIcuMcp.Models;
 using ModelContextProtocol.Server;
@@ -6,17 +6,12 @@ using ModelContextProtocol.Server;
 namespace IntervalsIcuMcp.McpServer;
 
 [McpServerToolType]
-public class WorkoutGeneratorMcpTool
+public class WorkoutGeneratorMcpTool(IWorkoutGeneratorService service)
 {
-    private readonly IWorkoutGeneratorService _service;
-
-    public WorkoutGeneratorMcpTool(IWorkoutGeneratorService service)
-    {
-        _service = service;
-    }
+    private readonly IWorkoutGeneratorService _service = service;
 
     [McpServerTool]
-    [Description("Generates a workout for a given sport, using HR zones, power zones, or RPE. Specify sport, title, description, and a list of intervals. Estimates TSS using the athlete profile from Intervals.icu.")]
+    [Description("Generates a workout for a given sport, using HR zones, power zones, or RPE. Specify sport, title, description, and a list of intervals. Estimates TSS using the athlete profile from Intervals.icu. You do not need to get the athlete profile yourself before using this function.")]
     public async Task<Workout> GenerateWorkoutAsync(
         [Description("The sport type for the workout (e.g., Ride, Run, Swim)")] SportType sport,
         [Description("The title of the workout")] string title,
